c2cc53f76f7f1c2fee15c9552233c50eca9d82b9
karthik <karthik35314@gmail.com>
2023-08-13 00:56:21 +0530

    first-commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserSchema } from './user.models';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://karthik_204:tests@cluster0.6z5m2kk.mongodb.net/account?retryWrites=true&w=majority'),
+    MongooseModule.forFeature([{name:'user',schema:UserSchema}])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
